ed1252f4bed383b42a4f961a610de60c138072ac
Quarante-deux-tssr <99953632+Quarante-deux-tssr@users.noreply.github.com>
2022-03-03 17:01:33 +0100

    test les types d'écriture de variable

---
+++ testvar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+dat=pwd
+
+echo 'dat'
+echo dat
+echo ''
+echo '$dat'
+echo $dat
+echo ''
+echo '${dat}'
+echo ${dat}
+echo ''
+echo '$(dat)'
+echo $(dat)
+echo ''
+echo '${$dat}'
+echo ${$dat}
+echo ''
+echo '$($dat)'
+echo $($dat)
+echo ''
+echo '$data' 
+echo $data 
+echo ''
+echo '${dat}a'
+echo ${dat}a
+echo ''
